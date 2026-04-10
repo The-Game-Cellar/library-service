338c7316061904241e66fa54d44242b41f17dcf0
Alex <alexander.westerberg.svedberg@gmail.com>
2026-04-10 12:16:50 +0200

    initial setup with application config, JPA

---
+++ src/main/java/com/thegamecellar/libraryservice/model/entity/UserPlatform.java
@@ -0,0 +1,27 @@
+package com.thegamecellar.libraryservice.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user_platforms")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserPlatform {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(name = "platform_name", nullable = false)
+    private String platformName;
+
+    @Column(name = "is_primary")
+    private Boolean isPrimary;
+}
